809527e92ade6d723505442f12780a3983d0b9ad
silvercho <tjddms01212@gmail.com>
2023-12-21 11:36:52 +0900

    setting

---
+++ src/test/java/com/ll/mb/MbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.mb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
